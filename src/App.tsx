9bdac6e3b6a7724fa07e688cc306c7778946a873
fouadhk <fouad.heri@gmail.com>
2019-09-13 15:54:49 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import logo from './logo.svg';
+import styled from 'styled-components';
+import { Avatar, Form, Button, List, Input, Tag, Row, Col, Layout, Icon } from 'antd';
+import Card from './Components/Card';
+import Comments from './Components/Comment'
+import "antd/dist/antd.css";
+import "./index.css";
+import './App.css';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+    <Layout>
+      <Sider className="bg-white"></Sider>
+      <Layout>
+        <Header className="bg-white"></Header>
+
+
+        <Content className="content">
+
+          <div className="container">
+            <a className="fil-ariane">Avantages</a>
+            <Row>
+              <Col span={8}>
+                <Title>Subvention Sports</Title>
+              </Col>
+              <Col span={8} offset={8}>
+                <Button className="btn-edit" type="primary"><Icon type="edit" />Éditer</Button>
+              </Col>
+            </Row>
+
+
+            <article className="article">
+              <Row gutter={16}>
+                <Col>
+                  <ArticleMedia className="article__Media">
+                    <img src="http://placehold.it/128x128"/>
+                  </ArticleMedia>
+                </Col>
+                <Col >
+                  <Row>
+                    <Tag color="magenta">Subvention</Tag>
+                    <Icon type="calendar" style={{color: "#FFD666"}}/><TextGrey className="txt-grey">Inscriptions: 01/01/2019 - 27/12/2019</TextGrey>
+                    <Icon type="clock-circle" style={{color: "#079CEE"}}/><TextGrey className="txt-grey">165 jours restants avant la fermeture</TextGrey>
+                  </Row>
+                  <Row>
+                      <ArticleParagraph>Remboursement des activités sportives des salariés à hauteur de 150€ sur présentation de factures nominatives de licence sportive, d’adhésion à un club de sport ou d'achat de billetterie pour des événements sportifs.</ArticleParagraph>
+                  </Row>
+                </Col>
+              </Row>
+            </article>
+
+            <div className="cards-list">
+              <Row gutter={16}>
+                <Col span={4}>
+                  <Card type="user" color="#3DD598" price="150" text="Par salarié" />
+                </Col>
+                <Col span={4}>
+                  <Card type="heart" color="#F5222D" price="100" text="Par conjoint" />
+                </Col>
+                <Col span={4}>
+                  <Card type="user-add" color="#EB2F96" price="100" text="Par enfant" />
+                </Col>
+                <Col span={4}>
+                  <Card type="usergroup-add" color="#722ED1" price="214" text="Participants" />
+                </Col>
+                <Col span={4}>
+                  <Card type="euro" color="#FA8C16" price="16 124" text="Consommés" />
+                </Col>
+              </Row>
+            </div>
+
+            <Row>
+              <div>
+                <h3>Commentaires</h3>
+                <Comments/>
+              </div>
+            </Row>
+
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+
+    </div>
+  );
+}
+
+const Title = styled.h1`
+  font-size: 30px;
+  line-height: 30px;
+`
+
+const ArticleMedia = styled.div`
+  width: fit-content;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const ArticleParagraph = styled.p`
+  color: #000;
+  margin-top: 22px;
+`
+
+const TextGrey = styled.span`
+  font-size: 14px;
+  line-height: 22px;
+  color: #8C8C8C;
+  margin: 0 20px 0 4px;
+`
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Col, Layout, Icon } from 'antd';
+
+class Card extends React.Component {
+  constructor(props) {
+    console.log("RENDER")
+    super(props)
+  }
+  _convertToRgba(hex) {
+    /**
+      Fonction qui va me permettre de récupérer une couleur au format hexagonal et la convertir en rbga.
+    */
+    var c;
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            c= hex.substring(1).split('');
+            if(c.length== 3){
+                c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+            }
+            c= '0x'+c.join('');
+            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.1)';
+        }
+        throw new Error('Bad Hex');
+  }
+
+  render() {
+    return (
+      <CardContainer>
+        <Col span={7}>
+          <Figure style={{background:this._convertToRgba(this.props.color)}}>
+            <Icon type={this.props.type} theme="outlined" style={{color: this.props.color}}/>
+          </Figure>
+        </Col>
+        <Col span={17}>
+          <Price>{this.props.price}€</Price>
+          <Text>{this.props.text}</Text>
+        </Col>
+      </CardContainer>
+    )
+  }
+}
+
+const CardContainer = styled.div`
+  width: 180px;
+  height: 100px;
+  display: inline-block;
+  background: #FFFFFF;
+  border: 1px solid rgba(192, 196, 207, 0.3);
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.03);
+  border-radius: 12px;
+  margin-top: 22px;
+  margin-bottom: 22px;
+  margin-right: 18px;
+  & > div {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+
+const Figure = styled.figure`
+  width: 36px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+const Price = styled.h4`
+  line-height: 100px;
+  font-size: 20px;
+  line-height: 32px;
+  color: #171725;
+  margin-bottom: 0;
+`;
+
+const Text = styled.span`
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.1px;
+`;
+
+export default Card
